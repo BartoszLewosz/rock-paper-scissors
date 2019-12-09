de6900f63743f046ebb7d8768a48c1f29cdcdb85
restaurant <restaurant@warrenhouse.local>
2019-12-09 17:13:21 +0000

    Add variables used in game

--- rock-paper-scissors/rock-paper-scissors/Program.cs
+++ rock-paper-scissors/rock-paper-scissors/Program.cs
@@ -6,7 +6,19 @@ namespace rock_paper_scissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Game rules.");
+
+            string player_one_choice, computer_choice;
+            bool play_again = true;
+            int score_player = 0;
+            int score_computer_choice = 0;
+            int draw = 0;
+            Random rnd = new Random();
+
+            while (play_again)
+            {
+
+            }
         }
     }
 }
